a5ecf31d27c2fffd7f45d36a490b83d8307035b6
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2023-07-31 23:52:58 -0600

    deploy github

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const PORT = process.env.PORT || 4000;
 
-export const PG_PORT = process.env.PG_PORT || 5432;
-export const PG_HOST = process.env.PG_HOST || "localhost";
-export const PG_DATABASE = process.env.PG_DATABASE || "tasksDB";
-export const PG_USER = process.env.PG_USER || "postgres";
-export const PG_PASSWORD = process.env.PG_PASSWORD || "ilhuicatzi.root";
+export const PGPORT = process.env.PGPORT || 5432;
+export const PGHOST = process.env.PGHOST || "localhost";
+export const PGDATABASE = process.env.PGDATABASE || "tasksDB";
+export const PGUSER = process.env.PGUSER || "postgres";
+export const PGPASSWORD = process.env.PGPASSWORD || "ilhuicatzi.root";
 
 export const ORIGIN = process.env.ORIGIN || "http://localhost:5173";
