02e6631d06b36ca9261893898ce87b1b24993a19
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2023-08-01 00:23:48 -0600

    URL db added for railway

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
 import pg from "pg";
-import { PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } from "./config.js";
+import { PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD } from "./config.js";
 
 export const pool = new pg.Pool({
-  host: PG_HOST,
-  port: PG_PORT,
-  user: PG_USER,
-  password: PG_PASSWORD,
-  database: PG_DATABASE,
+  host: PGHOST,
+  port: PGPORT,
+  user: PGUSER,
+  password: PGPASSWORD,
+  database: PGDATABASE,
 });
 
 pool.on("connect", () => console.log("Database connected successfully :)"));
